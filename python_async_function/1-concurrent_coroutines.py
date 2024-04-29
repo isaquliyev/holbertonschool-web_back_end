0275f2120d5f5f4fe9187bf81d70bcf9c8e7de58
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-29 18:23:39 +0400

    Now, wait_list is in ascending order

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,4 +10,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     wait_list = []
     for i in range(n):
         wait_list.append(await wait_random(max_delay))
-    return wait_list
+    return sorted(wait_list)
