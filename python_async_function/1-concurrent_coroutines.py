7ebcf23fe0011d7487a78a6ca07cdfa88cceef6d
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-29 18:35:21 +0400

    Awaiting after the creation of list

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Let's execute multiple coroutines at the same time with async"""
 
+
 from typing import List
+from asyncio import gather
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -9,5 +11,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """running wait_random for n times"""
     wait_list = []
     for i in range(n):
-        wait_list.append(await wait_random(max_delay))
+        wait_list.append(wait_random(max_delay))
+    wait_list = await gather(*wait_list)
     return sorted(wait_list)
