74dfa83b2e311df6fc6353aeba18ec864f5cf38b
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-22 01:23:11 +0400

    Finally I understand

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const newPromise = Promise.all([uploadPhoto, createUser]);
+  const newPromise = Promise.all([uploadPhoto(), createUser()]);
   return newPromise
     .then((body) => {
       const [photo, user] = body;
-      console.log(`${photo.value.body} ${user.value.firstName} ${user.value.lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
