4f1e6862d83a5b3d4daa4b53b029d689a08b400c
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-21 16:50:24 +0400

    Lowercase :)

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Success' });
+    if (success) resolve({ status: 200, body: 'success' });
     reject(new Error('The fake API is not working currently'));
   });
   promise.then(console.log('Got a response from the API'));
