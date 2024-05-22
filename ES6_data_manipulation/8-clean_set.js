f223116d15b5b9ec1e13f2a342eba537d6e89e9c
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-23 01:35:42 +0400

    startString should be string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (typeof startString !== 'string' || startString === '') return '';
   return [...set].reduce((acc, currentIndex) => {
     if (currentIndex.startsWith(startString)) return acc.concat(currentIndex.slice(startString.length), '-');
     return acc;
