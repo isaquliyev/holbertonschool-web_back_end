3c8f249e6324d98b1419693c7b2fd58265c9db2f
isaquliyev <isa.quluyev.wip@gmail.com>
2024-05-22 15:42:04 +0400

    Eslint fixed

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,14 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises).then((results) =>
-    results.map((result) => {
-      if (result.status === 'rejected') {
-	return { status: 'rejected', value: `Error: ${result.reason.message}`};
-      }
-      return result;
-    })
-  );
+  return Promise.allSettled(promises).then((results) => results.map((result) => {
+    if (result.status === 'rejected') return { status: 'rejected', value: `Error: ${result.reason.message}` };
+    return result;
+  }));
 }
-
